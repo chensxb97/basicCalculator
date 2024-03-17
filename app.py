e06fb3d9473f203a6c5ffd6ef0ae54aba88122b4
Benedict <chensxb97@gmail.com>
2024-03-18 00:33:53 +0800

    added click handlers using javascript

--- app.py
+++ app.py
@@ -1,31 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import re
 
 app = Flask(__name__)
 
 @app.route('/')
-def welcome():
+def index():
     return render_template('calculator.html')
 
-@app.route('/', methods=['POST'])
-def result():
-    var_1 = request.form.get("var_1", type=int, default=0)
-    var_2 = request.form.get("var_2", type=int, default=0)
-    operation = request.form.get("operation")
-    if(operation == 'Addition'):
-        result = var_1 + var_2
-    elif(operation == 'Subtraction'):
-        result = var_1 - var_2
-    elif(operation == 'Multiplication'):
-        result = var_1 * var_2
-    elif(operation == 'Division'):
-    	if(var_1==0 and var_2==0):
-    		result = 0
-    	else:
-        	result = var_1 / var_2
-    else:
-        result = 0
-    entry = result
-    return render_template('form.html', entry=entry)
 
 if __name__ == '__main__':
     app.run(debug=True)
