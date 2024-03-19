1207e2f6ae670295a47987dc27b860d16937420b
Benedict <chensxb97@gmail.com>
2024-03-19 22:03:44 +0800

    Added computation and error handling logic in app.py

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template, request, jsonify
-import re
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('calculator.html')
+def calculator():
+    return render_template('calculator.html',result="")
 
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    expression = request.form['expression']
+    try:
+        result = eval(expression)
+    except Exception:
+        return render_template('calculator.html', result='Err')
+    return render_template('calculator.html', result=str(result))
 
 if __name__ == '__main__':
     app.run(debug=True)
